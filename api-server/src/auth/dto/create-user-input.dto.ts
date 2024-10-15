11e036f0d0d7ac14888078ed25107ef32f30cf9d
Dmytro Sharshakov <113136582+Picmaster911@users.noreply.github.com>
2024-10-15 22:35:08 +0300

    Add Swager

--- api-server/src/auth/dto/create-user-input.dto.ts
+++ api-server/src/auth/dto/create-user-input.dto.ts
@@ -1,16 +1,30 @@
 import { IsInt, IsString } from 'class-validator';
 import { IRegisterUser } from '../interfaces/registerUser.interfaces';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserInputDto implements IRegisterUser {
+  @ApiProperty({ example: 'John Doe', description: 'The name of the user' })
   @IsString()
   username: string;
 
+  @ApiProperty({
+    example: 'sample@gmail.com',
+    description: 'The email of the user',
+  })
   @IsString()
   email: string;
 
+  @ApiProperty({
+    example: 'password123',
+    description: 'The password of the user',
+  })
   @IsString()
   password: string;
 
+  @ApiProperty({
+    example: '34',
+    description: 'The age of the user',
+  })
   @IsInt()
   age: number;
 }
--- api-server/src/main.ts
+++ api-server/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,15 @@ async function bootstrap() {
       forbidNonWhitelisted: false,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Hillel curse API')
+    .setDescription('The API HOME WORK')
+    .setVersion('1.0')
+    .addTag('API Hillel')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
